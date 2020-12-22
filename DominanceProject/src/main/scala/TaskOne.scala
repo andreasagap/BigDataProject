2df26343928141199014236ad9400cc4867ff41e
Andreas <andreasagap@gmail.com>
2020-12-23 00:20:45 +0200

    Dataframe Dim

--- DominanceProject/src/main/scala/TaskOne.scala
+++ DominanceProject/src/main/scala/TaskOne.scala
@@ -1,12 +1,46 @@
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.SparkContext
-import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions.{col, desc, lit, size}
+import org.apache.spark.sql.{DataFrame, Row, SparkSession}
+import org.apache.spark.sql.functions.{col, desc, lit, size, when}
 
 import scala.collection.mutable
+import scala.collection.mutable.ArrayBuffer
 import scala.math.BigDecimal.int2bigDecimal
 object TaskOne {
 
+  def isDominated(x: Array[Double], y:Array[Double]): Boolean = {
+    return isSmaller(x,y) & isSmallerEqual(x,y)
+  }
+
+  def isSmaller(x: Array[Double], y:Array[Double]):Boolean = {
+    val size = x.length
+    var flag = false
+    var i = 0
+    for (i <-0 to size -1) {
+      if (x(i) < y(i))
+        flag = true
+    }
+    return flag
+  }
+
+  def isSmallerEqual(x: Array[Double], y:Array[Double]):Boolean = {
+    val size = x.length
+    var flag = true
+    var i = 0
+    for (i <- 0 to size - 1) {
+      if (x(i) > y(i))
+        flag = false
+    }
+    return flag
+  }
+  def SFSkylineCalculation(dataframe: DataFrame,dimensions: Int):DataFrame = {
+
+//    dataframe.first().select("R",when(dataframe.first().)
+//    dataframe.foreach { row =>
+//      row
+//    }
+    return dataframe
+  }
 
   def main(args : Array[String]): Unit ={
     Logger.getRootLogger.setLevel(Level.INFO)
@@ -19,14 +53,16 @@ object TaskOne {
     // This import is needed
     import ss.implicits._
     val newDf = splitRDD.toDF("points")
-    val maxCols = newDf.limit(1).select("points").collectAsList().get(0).getList(0).size()
-    var result = newDf.select((0 until maxCols).map(i => $"points"(i).as(s"dim_$i")): _*)
+    val dimensions = newDf.limit(1).select("points").collectAsList().get(0).getList(0).size()
+    var result = newDf.select((0 until dimensions).map(i => $"points"(i).as(s"dim_$i")): _*)
 
     result = result.withColumn("SUM", result.columns.map(c => col(c)).reduce((c1, c2) => c1 + c2))
     result = result.sort(desc("SUM"))
 
     result = result.withColumn("R", lit(0))
-    result = result.withColumn("T", lit(0))
-    result.show(5)
+    //result = result.withColumn("T", lit(0))
+    result.show(4)
+    val mainMemorySkylines = SFSkylineCalculation(result,dimensions)
+    mainMemorySkylines.take(2).foreach(println)
   }
 }
